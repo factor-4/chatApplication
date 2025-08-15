d21f5330f4f96ac4b119b67f8ea7f48d0f596bab
factor-4 <snk.sojib@gmail.com>
2025-08-15 16:06:59 +0300

    adding username to web socket

--- src/main/java/com/khan/chat/chat/ChatController.java
+++ src/main/java/com/khan/chat/chat/ChatController.java
@@ -3,6 +3,7 @@ package com.khan.chat.chat;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -15,6 +16,21 @@ public class ChatController {
              @Payload ChatMessage chatMessage
     ){
 
+        return chatMessage;
+
+    }
+
+
+    @MessageMapping("/chat.addUser")
+    @SendTo("/topic/public")
+    public ChatMessage addUser (
+            @Payload ChatMessage chatMessage,
+            SimpMessageHeaderAccessor headerAccessor
+
+    ){
+
+        // add username in websocket session
+        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
         return chatMessage;
 
     }
