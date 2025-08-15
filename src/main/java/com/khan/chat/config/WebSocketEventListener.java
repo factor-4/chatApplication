adc55889f6c43255ddf204d3f234604b5145fe0b
factor-4 <snk.sojib@gmail.com>
2025-08-15 17:58:14 +0300

    user left message logic

--- src/main/java/com/khan/chat/config/WebSocketEventListener.java
+++ src/main/java/com/khan/chat/config/WebSocketEventListener.java
@@ -1,8 +1,12 @@
 package com.khan.chat.config;
 
+import com.khan.chat.chat.ChatMessage;
+import com.khan.chat.chat.MessageType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
@@ -12,11 +16,24 @@ import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
 public class WebSocketEventListener {
 
+    private final SimpMessageSendingOperations messageTemplate;
+
     @EventListener
     public void handleWebSocketDisconnectListener
             (SessionDisconnectEvent event)
     {
-        // TOdo implemented later
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String username= (String) headerAccessor.getSessionAttributes().get("username");
+
+        if(username != null ) {
+            log.info("user disconnected; {} ", username);
+            var chatMessage = ChatMessage.builder()
+                    .type(MessageType.LEAVE)
+                    .sender(username)
+                    .build();
+
+            messageTemplate.convertAndSend("/topic/public", chatMessage);
+        }
 
     }
 }
